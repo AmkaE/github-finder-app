ff6c0339201e16e483f5b34b10127cb03f89715a
Amka <Aminhassen1470@gmail.com>
2022-06-28 11:59:45 -0400

    create Navbar Component, install react-icons, react-router-dom, and prop-types.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
 const App = () => {
-	return <div>App</div>;
+	return (
+		<Router>
+			<div className='flex flex-col justify-between h-screen'>
+				<Navbar />
+				<main>Content</main>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
