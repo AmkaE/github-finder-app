b56dc576bcbaa0f85172eb7f26b59399fc3348ea
Amka <Aminhassen1470@gmail.com>
2022-08-13 14:13:34 -0400

    create SearchUsers component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import UserResults from '../components/users/UserResults';
+import UserSearch from '../components/users/UserSearch';
 
 const Home = () => {
 	return (
 		<>
-			{/* @TODO: User search component */}
+			<UserSearch />
 			<UserResults />
 		</>
 	);
